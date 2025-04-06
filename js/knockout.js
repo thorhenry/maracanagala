fb05d0a35d3433d6d1e2d749620556f7daa64944
Thorhenry <113508213+thorhenry@users.noreply.github.com>
2025-04-06 18:55:02 +0300

    Update knockout.js

--- js/knockout.js
+++ js/knockout.js
@@ -5,9 +5,9 @@ const knockoutData = {
             id: 'sf1',
             homeTeam: 'Mbale', // 1st in table
             awayTeam: 'Masaka',    // 3rd in table
-            homeScore: 0,
+            homeScore: 1,
             awayScore: 0,
-            status: 'live',
+            status: 'completed',
             date: '2025-04-06',
             time: '17:00',
             stadium: 'Maracaná Stadium'
